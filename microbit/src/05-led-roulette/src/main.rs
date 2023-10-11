6b8de4fe3ca80102d89a3ad2fb8b795f7da0df6b
Gabe Venberg <gabevenberg@gmail.com>
2023-10-10 22:43:08 -0500

    did led-roulette.

--- microbit/src/05-led-roulette/src/main.rs
+++ microbit/src/05-led-roulette/src/main.rs
@@ -3,15 +3,88 @@
 #![no_std]
 
 use cortex_m_rt::entry;
+use microbit::board::Board;
+use microbit::display::blocking::Display;
+use microbit::hal::timer::Timer;
 use panic_halt as _;
-use microbit as _;
+
+const DELAY: u32 = 32;
+
+#[derive(Debug, Default)]
+enum Direction {
+    #[default]
+    Left,
+    Down,
+    Right,
+    Up,
+}
+
+#[derive(Debug, Default)]
+struct State {
+    direction: Direction,
+    position: u8,
+}
+
+impl State {
+    fn advance(&mut self) {
+        if self.position >= 4 {
+            match self.direction {
+                Direction::Left => {
+                    self.direction = Direction::Down;
+                }
+                Direction::Down => {
+                    self.direction = Direction::Right;
+                }
+                Direction::Right => {
+                    self.direction = Direction::Up;
+                }
+                Direction::Up => {
+                    self.direction = Direction::Left;
+                }
+            }
+            self.position = 0;
+        }
+        self.position += 1
+    }
+
+    fn x(&self) -> usize {
+        match self.direction {
+            Direction::Left => 0,
+            Direction::Down => self.position as usize,
+            Direction::Right => 4,
+            Direction::Up => 4 - self.position as usize,
+        }
+    }
+
+    fn y(&self) -> usize {
+        match self.direction {
+            Direction::Left => self.position as usize,
+            Direction::Down => 4,
+            Direction::Right => 4 - self.position as usize,
+            Direction::Up => 0,
+        }
+    }
+}
 
 #[entry]
 fn main() -> ! {
-    let _y;
-    let x = 42;
-    _y = x;
+    let board = Board::take().unwrap();
+    let mut timer = Timer::new(board.TIMER0);
+    let mut display = Display::new(board.display_pins);
+    let mut leds = [
+        [0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0],
+    ];
 
-    // infinite loop; just so we don't leave this stack frame
-    loop {}
+    let mut state: State = Default::default();
+
+    loop {
+        leds[state.x()][state.y()] = 0;
+        state.advance();
+        leds[state.x()][state.y()] = 1;
+        display.show(&mut timer, leds, DELAY)
+    }
 }
